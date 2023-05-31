9e673080170e34212cccb15884eca84906d87081
Youhei Tooyama <ytooyama@users.noreply.github.com>
2023-05-31 16:33:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ current_year = datetime.now().year
 
 #人を定義
 a = current_year - 1981
+dora = current_year - 2012
 
 #結果表示
 print('今年は', current_year, '年')
 print('Aさんは今年', a, '歳になります')
+print('doraさんは今年', dora, '歳になります')
